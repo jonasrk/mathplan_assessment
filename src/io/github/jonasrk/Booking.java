30f00f031624a68d42a7e512f47d343cfb4c2361
Jonas <jonas.kemper.biz@gmail.com>
2017-12-09 13:10:30 +0100

    booking class in a seperate file

---
+++ src/io/github/jonasrk/Booking.java
@@ -0,0 +1,50 @@
+package io.github.jonasrk;
+
+// define class for bookings
+
+import java.util.ArrayList;
+import java.util.Date;
+
+class Booking {
+    public String weekday, room, lecture_id, lecture_name;
+    public Date start_time, end_time;
+    public ArrayList<String> curricula = new ArrayList<String>();
+
+    public Booking(String id, String weekday, String room, Date start_time, Date end_time, String lecture_name) {
+        this.lecture_id = id;
+        this.weekday = weekday;
+        this.room = room;
+        this.start_time = start_time;
+        this.end_time = end_time;
+        this.lecture_name = lecture_name;
+    }
+
+    private Boolean time_of_day_has_conflict(Booking other){
+        if (other.end_time.before(this.start_time) | other.end_time.before(this.start_time)){
+            return false;
+        } else if (other.start_time.after(this.end_time) | other.start_time.equals(this.end_time)){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Boolean has_room_conflict(Booking other){
+        if (other.weekday.equals(this.weekday) & other.room.equals(this.room)){
+            return time_of_day_has_conflict(other);
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean has_curriculum_conflict(Booking other){
+        ArrayList<String> intersection = new ArrayList<String>(this.curricula);
+        intersection.retainAll(other.curricula);
+        if (intersection.size() > 0 & this.lecture_id != other.lecture_id & other.weekday.equals(this.weekday)){
+            return time_of_day_has_conflict(other);
+        } else {
+            return false;
+        }
+    }
+
+}
--- src/io/github/jonasrk/Main.java
+++ src/io/github/jonasrk/Main.java
@@ -25,53 +25,6 @@ public class Main {
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
         Document document = documentBuilder.parse(file);
 
-
-        // define class for bookings
-
-        class Booking {
-            public String weekday, room, lecture_id, lecture_name;
-            public Date start_time, end_time;
-            public ArrayList<String> curricula = new ArrayList<String>();
-
-            public Booking(String id, String weekday, String room, Date start_time, Date end_time, String lecture_name) {
-                this.lecture_id = id;
-                this.weekday = weekday;
-                this.room = room;
-                this.start_time = start_time;
-                this.end_time = end_time;
-                this.lecture_name = lecture_name;
-            }
-
-            private Boolean time_of_day_has_conflict(Booking other){
-                if (other.end_time.before(this.start_time) | other.end_time.before(this.start_time)){
-                    return false;
-                } else if (other.start_time.after(this.end_time) | other.start_time.equals(this.end_time)){
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-
-            public Boolean has_room_conflict(Booking other){
-                if (other.weekday.equals(this.weekday) & other.room.equals(this.room)){
-                    return time_of_day_has_conflict(other);
-                } else {
-                    return false;
-                }
-            }
-
-            public Boolean has_curriculum_conflict(Booking other){
-                ArrayList<String> intersection = new ArrayList<String>(this.curricula);
-                intersection.retainAll(other.curricula);
-                if (intersection.size() > 0 & this.lecture_id != other.lecture_id & other.weekday.equals(this.weekday)){
-                    return time_of_day_has_conflict(other);
-                } else {
-                    return false;
-                }
-            }
-
-        }
-
         // read in all bookings and curricula
 
         NodeList nodeList = document.getElementsByTagName("booking");
@@ -123,14 +76,10 @@ public class Main {
                         x.curricula.add(name);
                     }
                 });
-
             }
-
-
-
         }
 
-        // compare all lectures in a room with one another and check for conflicts
+        // compare all bookings with one another and check for conflicts
 
         for (int i = 0; i < bookings.size(); i++) {
             for (int j = i + 1; j < bookings.size(); j++) {
